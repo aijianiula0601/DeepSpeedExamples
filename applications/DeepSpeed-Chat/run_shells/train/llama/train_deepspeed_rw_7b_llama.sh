489a9a4b9d8809e4f9a47159719577e3252efe57
aijianiula <aijianiula0601@gmail.com>
2023-05-12 07:25:13 +0800

    update

--- applications/DeepSpeed-Chat/run_shells/train/llama/train_deepspeed_rw_7b_llama.sh
+++ applications/DeepSpeed-Chat/run_shells/train/llama/train_deepspeed_rw_7b_llama.sh
@@ -9,7 +9,7 @@ cd "$curdir" || exit
 cd ../../../
 
 base_dir="/mnt/cephfs/hjh/train_record/nlp/deepSpeedExamples/llama7b"
-out_dir="${base_dir}/outs/actor-models"
+out_dir="${base_dir}/outs/reward-models"
 mkdir -p $out_dir
 
 llama_model_name='huggyllama/llama-7b'
@@ -20,24 +20,46 @@ ZERO_STAGE=3
 #-----------------
 # step 1
 #-----------------
-cd training/step1_supervised_finetuning
+cd training/step2_reward_model_finetuning
 
 #CUDA_VISIBLE_DEVICES=4,5,6,7 \
+#deepspeed main_llama.py \
+#   --data_path ${data_path} \
+#   --data_split 2,4,4 \
+#   --model_name_or_path ${llama_model_name} \
+#   --per_device_train_batch_size 4 \
+#   --per_device_eval_batch_size 4 \
+#   --max_seq_len 512 \
+#   --learning_rate 9.65e-6 \
+#   --weight_decay 0. \
+#   --num_train_epochs 16 \
+#   --gradient_accumulation_steps 1 \
+#   --lr_scheduler_type cosine \
+#   --num_warmup_steps 0 \
+#   --seed 1234 \
+#   --zero_stage $ZERO_STAGE \
+#   --deepspeed \
+#   --output_dir ${out_dir} \
+#   --offload
+
+
 deepspeed main_llama.py \
    --data_path ${data_path} \
    --data_split 2,4,4 \
    --model_name_or_path ${llama_model_name} \
+   --num_padding_at_beginning 1 \
    --per_device_train_batch_size 4 \
    --per_device_eval_batch_size 4 \
    --max_seq_len 512 \
-   --learning_rate 9.65e-6 \
-   --weight_decay 0. \
-   --num_train_epochs 16 \
+   --learning_rate 5e-5 \
+   --weight_decay 0.1 \
+   --num_train_epochs 1 \
+   --disable_dropout \
    --gradient_accumulation_steps 1 \
    --lr_scheduler_type cosine \
    --num_warmup_steps 0 \
    --seed 1234 \
    --zero_stage $ZERO_STAGE \
    --deepspeed \
-   --output_dir ${out_dir} \
+   --output_dir $out_dir \
    --offload
